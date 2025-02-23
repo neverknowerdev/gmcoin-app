90d69d2e1333e56baf97fbc2e867b9a32965992c
NeverKnower <neverknowerdev@gmail.com>
2025-02-23 23:54:50 +0100

    use NEXT_PUBLIC_ prefix for env variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,12 @@
-export const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS || "";
-export const TWITTER_CLIENT_ID = process.env.TWITTER_CLIENT_ID || "";
+export const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "";
+export const TWITTER_CLIENT_ID = process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID || "";
 export const CONTRACT_ABI = [
   "function requestTwitterVerification(string calldata accessCodeEncrypted, string calldata userID) public",
   "function userByWallet(address wallet) public view returns (string memory)",
   "event TwitterVerificationResult(string indexed userID, address indexed wallet, bool isSuccess, string errorMsg)",
 ];
-export const  API_URL = process.env.API_URL || ""; //mainnet
-export const TOKEN_URL= process.env.TOKEN_URL || "";
+export const  API_URL = process.env.NEXT_PUBLIC_API_URL || ""; //mainnet
+export const TOKEN_URL= process.env.NEXT_PUBLIC_TOKEN_URL || "";
 
 export const CHAINS = [
   {
